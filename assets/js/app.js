6585b0eae281576d43c91c28d1f3e5e0f7c3720c
Louise Hayes <louise@deewhy.ie>
2017-11-03 12:15:05 -0400

    next train time showing HHMM format

--- assets/js/app.js
+++ assets/js/app.js
@@ -46,37 +46,38 @@ database.ref().on("child_added", function (childSnapshot) {
     var name = childSnapshot.val().name;
     var destination = childSnapshot.val().destination;
     var startTime = (childSnapshot.val().startTime);
-    console.log("startTime " + startTime);
+    console.log("startTime= " + startTime);
     var frequency = (childSnapshot.val().frequency);
 
     // set dateSTart to a date in function toDate
     var dateStart = toDate(startTime);
-    console.log(dateStart);
+    console.log("dateStart= " + dateStart);
     // this dateStart is now in right format to be compared with moment.
     var minutesSinceStart = moment().diff(dateStart, "minutes");
     // we now have the mins since the time the train started and now
-    console.log(minutesSinceStart);
+    console.log("minutesSinceStart= " + minutesSinceStart + "frequency= " + frequency);
     // get the remainder of 'minutesSinceStart' divided by 'frequency' (if diff = 0 
     // then next train is due now / else the next train is due in 'diff' minutes
-    var diff = minutesSinceStart % frequency;
-    // if diff = 0, (train due now) then nextTrainTime=0 , else nextTrainTime=(frequency-diff)
-    var nextTrain = diff ? frequency - diff : 0;
-    console.log(nextTrain);
+    var remainder = minutesSinceStart % frequency;
+    console.log("remainder = " + remainder);
+    // if remainder = 0, (train due now) then nextTrainTime=0 , else nextTrainTime=(frequency-remainder)
+    var nextTrain = remainder ? frequency - remainder : 0;
+    console.log("nextTrain= " + nextTrain);
     // creata a new date of now
     var nextTrainTime = new Date();
     // add the remaining mins (nextTrain) to nextTrainTime(time now) in a function addMinutes
     nextTrainTime = addMinutes(nextTrainTime, nextTrain);
     // set the seconds to 0 so that when train due now, it works!
     nextTrainTime.setSeconds(0);
-
-    console.log(nextTrainTime);
-
+    
+    console.log("nextTrainTime= " + nextTrainTime);
+    // var dueTime=checkDate(dateStart);
     // full list of items to the well
     var varDiv = $('<tr>');
     var td1 = $('<td>' + name + '</td>');
     var td2 = $('<td>' + destination + '</td>');
     var td3 = $('<td>' + frequency + '</td>');
-    var td4 = $('<td>' + nextTrainTime.toString() + '</td>');
+    var td4 = $('<td>' + nextTrainTime.getHours().toString() + ':' + (nextTrainTime.getMinutes()<10?'0':'') + nextTrainTime.getMinutes() + '</td>');
     var td5 = $('<td>' + nextTrain + '</td>');
 
     varDiv.append(td1);
@@ -96,12 +97,28 @@ database.ref().on("child_added", function (childSnapshot) {
 // set to today with a time of user input startTime ...thank you stackoverflow!!!
 function toDate(dStr) {
     var now = new Date();
-    console.log(now);
+    console.log("now= " + now);
+//    if start time is in future set 
+    if (dStr > now){
+
+    }
     now.setHours(dStr.substr(0, dStr.indexOf(":")));
     now.setMinutes(dStr.substr(dStr.indexOf(":") + 1));
     now.setSeconds(0);
     return now;
 }
+
+// function checkDate(dStr) {
+//     var now = new Date();
+//     console.log("now= " + now);
+// //    if start time is in future set due time to start time
+//     if (dStr > now){
+//         dueTime = dStr;
+//         return dueTime;
+//     };
+
+//     };
+
 // this function I believe takes the date/time as of now and 
 // adds the 'minutes' remaining until next train is due 
 // and returns the 'date' i.e. nextarrivaltime in a readable format
